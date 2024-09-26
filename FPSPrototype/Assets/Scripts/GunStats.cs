36a678079f6d079df18eaad149e23181c97e83d5
AYepezCampos <abelyepezcampos@gmail.com>
2024-09-25 22:43:33 -0500

    new scene

--- FPSPrototype/Assets/Scripts/GunStats.cs
+++ FPSPrototype/Assets/Scripts/GunStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,5 +18,8 @@ public class GunStats : ScriptableObject
     public AudioClip[] shootSound;
     public float shootVol;
 
-
+    internal void InitializeAmmo()
+    {
+        throw new NotImplementedException();
+    }
 }
